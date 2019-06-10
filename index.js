c446b4223c7a09f5dc0cfd1a8907a33928407c4f
Apple <apple@Apples-MacBook-Pro-3.local>
2019-06-10 11:42:25 -0700

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+document.getElementById('timer').innerHTML =
+ 03 + ":" + 00;
+startTimer();
+
+function startTimer() {
+ var presentTime = document.getElementById('timer').innerHTML;
+ var timeArray = presentTime.split(/[:]+/);
+ var m = timeArray[0];
+ var s = checkSecond((timeArray[1] - 1));
+ if(s==59){m=m-1}
+ //if(m<0){alert('timer completed')}
+
+ document.getElementById('timer').innerHTML =
+   m + ":" + s;
+ setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+ if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+ if (sec < 0) {sec = "59"};
+ return sec;
+}
+
 
 var cardContainer = $('.card-container');
 var submit = $('.submit-btn');
@@ -12,12 +35,11 @@ function newList(){
 function appendNewToDo(toDo){
  cardContainer.append(`
 <div class="to-do-card">
- <p>${toDo}</p>
- <input type= "checkbox"           class="checkbox">
+ <p>${toDo}</p><br>
+ <input type= "checkbox" class="checkbox">
 </div>
 
 `);
-
 
  var checkbox = $('.checkbox');
  checkbox.on("click", remove );
